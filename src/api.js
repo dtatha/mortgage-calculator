4bfb3783978c3b073d7798581d43fb15aba8380c
picodex <dtatha@gmail.com>
2024-07-01 20:35:27 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiEndpoint = '17mubt8b59.execute-api.us-east-1.amazonaws.com/deploy/calculateMortgage';
+const apiEndpoint = 'https://17mubt8b59.execute-api.us-east-1.amazonaws.com/deploy/calculateMortgage';
 
 export async function calculateMortgage(principal, annualInterestRate, years) {
     const response = await axios.post(apiEndpoint, {
